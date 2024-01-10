458c4e70e4ab7b25a99d332ebbf4ee3d027bde19
robertreingit <r.rein@dshs-koeln.de>
2024-01-10 09:53:49 +0100

    refactor: Add path for office.

--- scratch_01.py
+++ scratch_01.py
@@ -10,8 +10,15 @@ import matplotlib.pyplot as plt
 # :set visualbell
 # :set noerrorbells
 
-fpath = r"D:\sciebo\research_projects\DFL_position\data\raw\ObservedPositionalData\DFL-MAT-0002UK.xml"
-fpath_info = r"D:\sciebo\research_projects\DFL_position\data\raw\MatchInformation\DFL-MAT-0002UK.xml"
+# home
+root_path = r"D:\sciebo\research_projects\DFL_position\data"
+fpath = root_path + r"\raw\ObservedPositionalData\DFL-MAT-0002UK.xml"
+fpath_info = root_path + r"\raw\MatchInformation\DFL-MAT-0002UK.xml"
+
+# office
+root_path = r"D:\research_projects\DFG_soccer\DFL_Position"
+fpath = root_path + r"\ObservedPositionalData\DFL-MAT-0002WQ.xml"
+fpath_info = root_path + r"\MatchInformation\DFL-MAT-0002WQ.xml"
 
 
 (
@@ -19,13 +26,12 @@ fpath_info = r"D:\sciebo\research_projects\DFL_position\data\raw\MatchInformatio
     possession_objects,
     ballstatus_objects,
     teamsheets,
-    pitch
+    pitch,
 ) = dfl.read_position_data_xml(fpath, fpath_info)
 
 ax = plt.subplots()[1]
 pitch.plot(ax=ax)
 plt.show()
-
 
 xy1 = xy_objects["firstHalf"]["Home"]
 np.shape(xy1)
@@ -33,13 +39,13 @@ xy1 = filter.butterworth_lowpass(xy1, Wn=0.25)
 
 ax = plt.subplots()[1]
 pitch.plot(ax=ax)
-xy1.plot(t=(10, 10000), ax=ax, plot_type='trajectories')
+xy1.plot(t=(10, 10000), ax=ax, plot_type="trajectories")
 plt.show()
 
 p1 = xy1.player(1)
 p2 = xy1.player(3)
 ax = plt.subplots()[1]
 pitch.plot(ax=ax)
-ax.plot(p1[:, 0], p1[:, 1], color='blue')
-ax.plot(p2[:, 0], p2[:, 1], color='red')
+ax.plot(p1[:, 0], p1[:, 1], color="blue")
+ax.plot(p2[:, 0], p2[:, 1], color="red")
 plt.show()
