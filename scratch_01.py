e769ae4145b3b6087e7ea439e5fe86e4e6970d2c
robertreingit <r.rein@dshs-koeln.de>
2024-01-11 14:04:18 +0100

    refactor: calculate distances.

--- scratch_01.py
+++ scratch_01.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
 import floodlight.io.dfl as dfl
 import floodlight.transforms.filter as filter
 import matplotlib.pyplot as plt
+from floodlight.models.kinematics import DistanceModel
 
 # execute ALT+SHIFT+e
 # CTRL+TAB
@@ -19,6 +21,7 @@ fpath_info = root_path + r"\raw\MatchInformation\DFL-MAT-0002UK.xml"
 root_path = r"D:\research_projects\DFG_soccer\DFL_Position"
 fpath = root_path + r"\ObservedPositionalData\DFL-MAT-0002WQ.xml"
 fpath_info = root_path + r"\MatchInformation\DFL-MAT-0002WQ.xml"
+fpath_evt = root_path + r"\EventData\DFL-MAT-0002WQ.xml"
 
 
 (
@@ -50,6 +53,38 @@ ax.plot(p1[:, 0], p1[:, 1], color="blue")
 ax.plot(p2[:, 0], p2[:, 1], color="red")
 plt.show()
 
+
 # event data
-evts,_,_ = dfl.read_event_data_xml(fpath_evt, fpath_info, teamsheets['Home'],
-                                   teamsheets['Away'])
+evts, _, _ = dfl.read_event_data_xml(
+    fpath_evt, fpath_info, teamsheets["Home"], teamsheets["Away"]
+)
+evts["firstHalf"]["Home"].events.head()
+
+# calculate distance
+no_games = 10
+result_df = pd.DataFrame(
+    data={
+        "team_1_id": np.full(no_games, np.NAN),
+        "team_1_dist": np.full(no_games, np.NAN),
+        "team_2_id": np.full(no_games, np.NAN),
+        "team_2_dist": np.full(no_games, np.NAN),
+    }
+)
+
+dm = DistanceModel()
+
+current_game = 0
+total_distance = {"Home": 0, "Away": 0}
+for half in xy_objects:
+    for team in total_distance:
+        xy_tmp = xy_objects[half][team]
+        dm.fit(xy_tmp)
+        total_distance[team] += dm.cumulative_distance_covered()[-1, :].sum()
+team_id = {
+    "Home": teamsheets["Home"].teamsheet.team[0],
+    "Away": teamsheets["Away"].teamsheet.team[0],
+}
+result_df.loc[current_game, "team_1_id"] = team_id["Home"]
+result_df.loc[current_game, "team_1_dist"] = total_distance["Home"]
+result_df.loc[current_game, "team_2_id"] = team_id["Away"]
+result_df.loc[current_game, "team_2_dist"] = total_distance["Away"]
